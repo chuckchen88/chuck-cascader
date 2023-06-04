ac0e4d6626d119c3259f6e35554e4153430b5ec3
chuckchen <2811863274@qq.com>
2023-06-04 23:37:48 +0800

    打包docs

--- vite.config.ts
+++ vite.config.ts
@@ -30,16 +30,16 @@ export default defineConfig({
 	build: {
 			outDir: buidType, // 打包文件的输出目录
 			lib: (buidType === 'docs')?false:lib,
-			rollupOptions: {
-				// 确保外部化处理那些你不想打包进库的依赖
-				external: ["vue"],
-				output: {
-					// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-					globals: {
-						vue: "Vue",
-					},
-				},
-			}, // rollup打包配置
+			// rollupOptions: {
+			// 	// 确保外部化处理那些你不想打包进库的依赖
+			// 	external: ["vue"],
+			// 	output: {
+			// 		// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+			// 		globals: {
+			// 			vue: "Vue",
+			// 		},
+			// 	},
+			// }, // rollup打包配置
 		},
 	css: {
 		// css预处理器
